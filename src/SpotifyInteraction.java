f9ad51685840b90ce1974efa64b13b7398ad98eb
partharao <parthasprao@gmail.com>
2021-05-01 19:09:50 -0400

    changed the auth sequence
    aaaaaaa

--- src/SpotifyInteraction.java
+++ src/SpotifyInteraction.java
@@ -1,7 +1,7 @@
 import com.wrapper.spotify.SpotifyApi;
 import com.wrapper.spotify.exceptions.SpotifyWebApiException;
 import com.wrapper.spotify.model_objects.credentials.AuthorizationCodeCredentials;
-import com.wrapper.spotify.requests.authorization.authorization_code.AuthorizationCodeRefreshRequest;
+import com.wrapper.spotify.requests.authorization.authorization_code.pkce.AuthorizationCodePKCERefreshRequest;
 import org.apache.hc.core5.http.ParseException;
 
 import java.io.IOException;
@@ -11,24 +11,22 @@ import java.util.concurrent.CompletionException;
 
 public class SpotifyInteraction {
     private static final String clientId = "zyuxhfo1c51b5hxjk09x2uhv5n0svgd6g";
-    private static final String clientSecret = "zudknyqbh3wunbhcvg9uyvo7uwzeu6nne";
     private static final String refreshToken = "b0KuPuLw77Z0hQhCsK-GTHoEx_kethtn357V7iqwEpCTIsLgqbBC_vQBTGC6M5rINl0FrqHK-D3cbOsMOlfyVKuQPvpyGcLcxAoLOTpYXc28nVwB7iBq2oKj9G9lHkFOUKn";
 
     private static final SpotifyApi spotifyApi = new SpotifyApi.Builder()
             .setClientId(clientId)
-            .setClientSecret(clientSecret)
             .setRefreshToken(refreshToken)
             .build();
-
-    private static final AuthorizationCodeRefreshRequest authorizationCodeRefreshRequest = spotifyApi.authorizationCodeRefresh()
+    private static final AuthorizationCodePKCERefreshRequest authorizationCodePKCERefreshRequest = spotifyApi.authorizationCodePKCERefresh()
             .build();
 
     public static void authorizationCodeRefresh_Sync() {
         try {
-            final AuthorizationCodeCredentials authorizationCodeCredentials = authorizationCodeRefreshRequest.execute();
+            final AuthorizationCodeCredentials authorizationCodeCredentials = authorizationCodePKCERefreshRequest.execute();
 
             // Set access and refresh token for further "spotifyApi" object usage
             spotifyApi.setAccessToken(authorizationCodeCredentials.getAccessToken());
+            spotifyApi.setRefreshToken(authorizationCodeCredentials.getRefreshToken());
 
             System.out.println("Expires in: " + authorizationCodeCredentials.getExpiresIn());
         } catch (IOException | SpotifyWebApiException | ParseException e) {
