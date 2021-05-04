233aade823a121f9d98c917b9bc19504c3819390
partharao <parthasprao@gmail.com>
2021-05-04 18:15:45 -0400

    All functionality basically there
    Can get a ton of songs and their features, put them in adjacency list format, then create playlist with that.

--- src/SpotifyInteraction.java
+++ src/SpotifyInteraction.java
@@ -5,21 +5,26 @@ import com.wrapper.spotify.SpotifyApi;
 import com.wrapper.spotify.SpotifyHttpManager;
 import com.wrapper.spotify.exceptions.SpotifyWebApiException;
 import com.wrapper.spotify.model_objects.credentials.AuthorizationCodeCredentials;
-import com.wrapper.spotify.model_objects.specification.Paging;
-import com.wrapper.spotify.model_objects.specification.Track;
+import com.wrapper.spotify.model_objects.special.SnapshotResult;
+import com.wrapper.spotify.model_objects.specification.*;
 import com.wrapper.spotify.requests.authorization.authorization_code.AuthorizationCodeRefreshRequest;
 import com.wrapper.spotify.requests.authorization.authorization_code.AuthorizationCodeRequest;
 import com.wrapper.spotify.requests.authorization.authorization_code.AuthorizationCodeUriRequest;
-import com.wrapper.spotify.requests.authorization.authorization_code.pkce.AuthorizationCodePKCERefreshRequest;
+import com.wrapper.spotify.requests.data.browse.GetRecommendationsRequest;
+import com.wrapper.spotify.requests.data.playlists.AddItemsToPlaylistRequest;
+import com.wrapper.spotify.requests.data.playlists.CreatePlaylistRequest;
 import com.wrapper.spotify.requests.data.search.simplified.SearchTracksRequest;
+import com.wrapper.spotify.requests.data.tracks.GetAudioFeaturesForSeveralTracksRequest;
+import com.wrapper.spotify.requests.data.tracks.GetAudioFeaturesForTrackRequest;
+import com.wrapper.spotify.requests.data.users_profile.GetCurrentUsersProfileRequest;
 import org.apache.hc.core5.http.ParseException;
 
 import java.io.*;
 import java.net.*;
-import java.util.concurrent.CancellationException;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CompletionException;
-import java.awt.Desktop;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 
@@ -29,10 +34,10 @@ public class SpotifyInteraction {
     private static final String refreshToken = "b0KuPuLw77Z0hQhCsK-GTHoEx_kethtn357V7iqwEpCTIsLgqbBC_vQBTGC6M5rINl0FrqHK-D3cbOsMOlfyVKuQPvpyGcLcxAoLOTpYXc28nVwB7iBq2oKj9G9lHkFOUKn";
     private static URI redirectUri = SpotifyHttpManager.makeUri("http://localhost:8000/example");
     private static String code = "AQAWiI83i4qqzffEiGo5oI4ZabpfLGRlIG2oWwRxccGNv8gYCzoW9x8XpaaSd4Vd5yZsAGj5gcuBZRhJ4dm3_zQYA7XPZOhWgNOIFXKDF-vXkmcK_2S1e2cYW3A_BU6coMWjLGGp_g7Hc6OVAOVEkgksf_e6cJdbg16D8mnaNUa7VfgqMLWQo8uZ16EOuxos2nHm6YM";
-    private ServerSocket serverSocket;
-    private Socket clientSocket;
-    private PrintWriter out;
-    private BufferedReader in;
+    private static String sourceId;
+    private static Map<String, List<Float>> songAttributes;
+    private static Map<String, List<String>> songAdjacency;
+    private static Map<String, String> songUris;
 
     private static HttpServer server;
 
@@ -100,22 +105,125 @@ public class SpotifyInteraction {
         }
     }
 
-    public static void searchTracks_Sync() {
+    public static void getSourceTrack(String title, String artist) {
         try {
-            searchTracksRequest = spotifyApi.searchTracks("Blinding Lights")
+            searchTracksRequest = spotifyApi.searchTracks(title)
 //          .market(CountryCode.SE)
-//          .limit(10)
 //          .offset(0)
 //          .includeExternal("audio")
             .build();
             final Paging<Track> trackPaging = searchTracksRequest.execute();
-
-            System.out.println("Total: " + trackPaging.getTotal());
+            for (Track track : trackPaging.getItems()) {
+                if (track.getName().contentEquals(title) && track.getArtists()[0].getName().contentEquals(artist)) {
+                    sourceId = track.getId();
+                    System.out.println(track.getName());
+                    System.out.println(track.getId());
+                    break;
+                }
+                System.out.println(track.getName() + track.getArtists()[0].toString());
+            }
         } catch (IOException | SpotifyWebApiException | ParseException e) {
-            System.out.print("search tracks sync: ");
             System.out.println("Error: " + e.getMessage());
         }
     }
+
+    public static void getTracks() {
+        try {
+            List<String> trackIds = new ArrayList<String>();
+            songAttributes = new HashMap<String, List<Float>>();
+            songAdjacency = new HashMap<String, List<String>>();
+            songUris = new HashMap<String, String>();
+            trackIds.add(sourceId);
+            GetRecommendationsRequest recommendationsRequest;
+            List<String> adjacencies;
+            for (int i = 0; i < 31; i++) {
+                adjacencies = new ArrayList<String>();
+                TimeUnit.SECONDS.sleep(1);
+                recommendationsRequest = spotifyApi.getRecommendations()
+                        .seed_tracks(trackIds.get(i))
+                        .limit(5)
+                        .build();
+                Recommendations recommendations = recommendationsRequest.execute();
+                for (TrackSimplified track : recommendations.getTracks()) {
+                    String id = track.getId();
+                    adjacencies.add(id);
+                    trackIds.add(id);
+                }
+                songAdjacency.putIfAbsent(trackIds.get(i), adjacencies);
+            }
+            String[] songArray1 = trackIds.subList(0,50).toArray(new String[0]);
+            String[] songArray2 = trackIds.subList(50,100).toArray(new String[0]);
+            String[] songArray3 = trackIds.subList(50,100).toArray(new String[0]);
+            List<AudioFeatures> featuresList = new ArrayList<AudioFeatures>();
+            GetAudioFeaturesForSeveralTracksRequest audioFeaturesForSeveralTracksRequest1 = spotifyApi
+                    .getAudioFeaturesForSeveralTracks(songArray1)
+                    .build();
+            GetAudioFeaturesForSeveralTracksRequest audioFeaturesForSeveralTracksRequest2 = spotifyApi
+                    .getAudioFeaturesForSeveralTracks(songArray2)
+                    .build();
+            GetAudioFeaturesForSeveralTracksRequest audioFeaturesForSeveralTracksRequest3 = spotifyApi
+                    .getAudioFeaturesForSeveralTracks(songArray3)
+                    .build();
+            featuresList.addAll(List.of(audioFeaturesForSeveralTracksRequest1.execute()));
+            featuresList.addAll(List.of(audioFeaturesForSeveralTracksRequest2.execute()));
+            featuresList.addAll(List.of(audioFeaturesForSeveralTracksRequest3.execute()));
+            for (String id : trackIds) {
+                for (AudioFeatures features: featuresList) {
+                    if (id.contentEquals(features.getId())) {
+                        songUris.put(id, features.getUri());
+                        songAttributes.putIfAbsent(id, prepareFeatures(features));
+                    }
+                }
+            }
+        } catch (IOException | SpotifyWebApiException | ParseException | InterruptedException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    public static List<Float> prepareFeatures(AudioFeatures vector) {
+        List<Float> transformed = new ArrayList<Float>();
+        transformed.add(vector.getAcousticness());
+        transformed.add(vector.getDanceability());
+        transformed.add(vector.getEnergy());
+        transformed.add(vector.getValence());
+        Float tempoTransform = vector.getTempo() / 200;
+        if (tempoTransform < 1.0) {
+            transformed.add(tempoTransform);
+        } else {
+            transformed.add(1f);
+        }
+        return transformed;
+    }
+
+    public static void createPlaylist(List<String> finalIdList) {
+        try {
+            List<String> playlistURIs = new ArrayList<String>();
+            for (String id : finalIdList) {
+                playlistURIs.add(songUris.get(id));
+            }
+            GetCurrentUsersProfileRequest currentUsersProfileRequest = spotifyApi.getCurrentUsersProfile().build();
+            String userId = currentUsersProfileRequest.execute().getId();
+            CreatePlaylistRequest createPlaylistRequest = spotifyApi
+                    .createPlaylist(userId, "Custom Playlist for you")
+                    .collaborative(false)
+                    .public_(true)
+                    .build();
+            String playlistId = createPlaylistRequest.execute().getId();
+            AddItemsToPlaylistRequest addItemsToPlaylistRequest = spotifyApi
+                    .addItemsToPlaylist(playlistId, playlistURIs.toArray(new String[0]))
+                    .build();
+            SnapshotResult result = addItemsToPlaylistRequest.execute();
+            System.out.println(result.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SpotifyWebApiException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 
 
     public static void main(String[] args) {
@@ -125,6 +233,7 @@ public class SpotifyInteraction {
                 .setRedirectUri(redirectUri)
                 .build();
         authorizationCodeUriRequest = spotifyApi.authorizationCodeUri()
+                .scope("user-read-private,user-read-email,playlist-modify-public,playlist-modify-private")
                 .show_dialog(true)
                 .build();
         authorizationCodeRefreshRequest = spotifyApi.authorizationCodeRefresh()
@@ -140,7 +249,12 @@ public class SpotifyInteraction {
         server.start();
         authorizationCodeUri_Sync();
         authorizationCode_Sync();
-        searchTracks_Sync();
+        getSourceTrack("Blinding Lights", "The Weeknd");
+        getTracks();
+        List<String> forPlaylist = new ArrayList<String>();
+        forPlaylist.add("0VjIjW4GlUZAMYd2vXMi3b");
+        createPlaylist(forPlaylist);
+        System.out.println(songAttributes.toString());
         server.stop(0);
     }
 
